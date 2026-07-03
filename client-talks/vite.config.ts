09c2b2b77609c8132b2739b742dc1e2b95bb3b30
Jonathan Branam <jonathan.branam@gmail.com>
2026-07-03 11:54:47 -0400

    Externalizes phaser for client-talks.

--- client-talks/vite.config.ts
+++ client-talks/vite.config.ts
@@ -20,6 +20,9 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      external: ['phaser'],
+    },
   },
   server: {
     port: 6055,
